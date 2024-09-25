b2b4ec7055e60dac501838349b8782dc279e1248
Francis <business.francis300@gmail.com>
2024-09-25 14:37:42 +0800

    feat: Added BaseScreen class

---
+++ BaseScreen.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "AConsole.h"
+#include <memory>
+class BaseScreen : public AConsole
+{
+public:
+	BaseScreen(String processName, std::shared_ptr<String>);
+	void onEnabled() override;
+	void process() override;
+	void display() override;
+
+private:
+	void printProcessInfo();
+	std::shared_ptr<String> attachedProcess;
+	bool refreshed = false;
+};
+
