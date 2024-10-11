95528d3d70c7bbd803f0765dd2363308ced273dd
Francis <business.francis300@gmail.com>
2024-10-11 19:51:05 +0800

    feat: added interval beteen dummy processes

--- main.cpp
+++ main.cpp
@@ -11,11 +11,14 @@ int main()
 	ConsoleManager::initialize();
 	Scheduler::initialize(Scheduler::FCFS, 4, 300);
 
+	std::cout << "Generating dummy processes. . .\n";
 	for (int i = 0; i < 10; ++i) {
 		std::string processName = "Process_" + std::to_string(i + 1).substr(0, 2);
 		auto process = std::make_shared<Process>(processName, i, 100, PrintCommand());
 		Scheduler::getInstance()->addNewProcess(process);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	}
+	std::cout << "Generated \n";
 
 	bool running = true;
 	while (running)
