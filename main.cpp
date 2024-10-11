64027c8fe5dbb589bc4b07c02686309e75176f43
Francis <business.francis300@gmail.com>
2024-10-11 19:52:33 +0800

    style: changed text for generating dummy processes

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ int main()
 	ConsoleManager::initialize();
 	Scheduler::initialize(Scheduler::FCFS, 4, 300);
 
-	std::cout << "Generating dummy processes. . .\n";
+	std::cout << "Generating dummy processes please wait. . .\n";
 	for (int i = 0; i < 10; ++i) {
 		std::string processName = "Process_" + std::to_string(i + 1).substr(0, 2);
 		auto process = std::make_shared<Process>(processName, i, 100, PrintCommand());
 		Scheduler::getInstance()->addNewProcess(process);
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	}
-	std::cout << "Generated \n";
+	std::cout << "Generated dummy processes \n";
 
 	bool running = true;
 	while (running)
