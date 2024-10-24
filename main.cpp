d56c03a1c7b8160f3840d87fac7ff1c78d5af342
xLelouch03 <hans_rejano@dlsu.edu.ph>
2024-10-25 00:25:48 +0800

    style: removed unused code in main.cpp

--- main.cpp
+++ main.cpp
@@ -11,16 +11,6 @@ int main()
 	ConsoleManager::initialize();
 	Scheduler::initialize(RR, 5, 4, 100000);
 
-	//std::cout << "Generating dummy processes please wait. . .\n";
-	//for (int i = 0; i < 20; ++i) {
-	//	std::string processName = "Process_" + std::to_string(i + 1).substr(0, 2);
-	//	String toPrint = "Hello world from " + processName;
-	//	auto process = std::make_shared<Process>(processName, i, 100, PrintCommand(toPrint));
-	//	Scheduler::getInstance()->addNewProcess(process);
-	//	//std::this_thread::sleep_for(std::chrono::milliseconds(500));
-	//}
-	//std::cout << "Generated dummy processes \n";
-
 	bool running = true;
 	while (running)
 	{
